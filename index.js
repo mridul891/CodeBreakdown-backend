24dda09f717340268d50ad32abb723f0b9f67f05
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-03-23 22:16:10 +0530

    minor changes

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/completions", async (req, res) => {
         })
     }
     try {
-        const response = await fetch('http://localhost:8080/completions', options)
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
         res.send(data)
     } catch (error) {
