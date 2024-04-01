5476a77bcdba13a9aad4990b22a441fae0cf0699
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-04-01 22:53:48 +0530

    minor

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -50,8 +50,7 @@ export const signin = async (req, res) => {
             expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
             httpOnly: true
         };
-        res.status(206).cookie("token", token, options);
-        res.status(201).json({ user: existingUser, token: token });
+        res.status(200).cookie("token", token, options).json({ user: existingUser, token: token });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "SomeThing got wrong" })
