b7eb2c8a9c7d8d7c5b472ce5ebaefb46d299f6c8
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-04-02 21:30:34 +0530

    minor changes

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -46,12 +46,12 @@ export const signin = async (req, res) => {
             return res.status(400).json({ message: "Invalid Credentials" });
         }
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, SECRET_KEY, { expiresIn: "30d" });
+        const token = await jwt.sign({ email: existingUser.email, id: existingUser._id }, SECRET_KEY, { expiresIn: "30d" });
         const options = {
             expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
             httpOnly: true
         };
-        res.cookie("tokens", token);
+        await res.cookie("tokens", token);
         res.json({ user: existingUser, token: token });
     } catch (error) {
         console.log(error);
